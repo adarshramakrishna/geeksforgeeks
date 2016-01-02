a7a75230c16d120de1b88d6575786ab8010917a9
gnijuohz <gnijuohz@gmail.com>
2016-01-01 21:14:32 -0700

    added two books

--- geeksforgeeks-book/copy_to_goodies.py
+++
@@ -1,8 +0,0 @@
-import sys
-import glob
-from subprocess import call
-
-if __name__ == "__main__":
-    for book_format in ['mobi', 'epub', 'docx']:
-        for book in glob.glob("./*/*." + book_format):
-            call("cp " + book + " ../goodies/geeksforgeeks/" + book_format, shell=True)
--- geeksforgeeks/update_book.sh
+++
@@ -1,9 +0,0 @@
-# this is a cron job script
-cd ~/Awesome/github/geeksforgeeks-books/geeksforgeeks
-python scrapy_books.py
-cd ../geeksforgeeks-book
-python batch_generate_book.py
-python copy_to_goodies.py
-git add -A
-git commit -m"updating books"
-git push
